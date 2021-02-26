33dbd06d1f052e12146636c504e4e9a55c646b82
JoaquinChartier <48659409+JoaquinChartier@users.noreply.github.com>
2021-02-25 21:20:20 -0300

    Changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+"use strict";
 window.onload = async () => {
-    let mtContractReader;
-    let mtContractSigner;
-    let userAddress;
+    let mtContractReader; //Can Read
+    let mtContractSigner; //Can Sign
+    let userAddress; //Metamask address
     const contractAddress = "0x82E60f74D7F809DB197428aFcD154314d140eFA0"; //MovingTruck contract address
     //Open metamask and load
     await window.ethereum.enable();
@@ -10,72 +11,58 @@ window.onload = async () => {
     //Load ERC20 ABI
     let data = await getJSON("../ABI/ERC20Preset.json");
     const erc20ABI = data.abi;
-
     function getJSON(url) {
         //Get JSON ABI
         return new Promise((resolve, reject) => {
             fetch(url)
                 .then((response) => {
-                    resolve(response.json());
-                })
+                resolve(response.json());
+            })
                 .then((data) => reject(data));
         });
     }
-
-    function verifyToken(tokenContractAddress) {
+    async function verifyToken(tokenContractAddress) {
         //Check available balance on specified token and approvals
-        let tokenContract = new ethers.Contract(
-            tokenContractAddress,
-            erc20ABI,
-            provider
-        );
-        let name = tokenContract.name();
-        let symbol = tokenContract.symbol();
+        let tokenContract = new ethers.Contract(tokenContractAddress, erc20ABI, provider);
+        let name = await tokenContract.name();
+        let symbol = await tokenContract.symbol();
         let balance = await tokenContract.balanceOf(userAddress);
         balance = ethers.utils.formatUnits(balance, 18);
-        let amountApproved = tokenContract.allowance(userAddress);
+        let amountApproved = await tokenContract.allowance(userAddress, contractAddress);
         console.log(amountApproved, name, symbol, balance);
     }
-
-    function moveTokens(ethValue,tokensArray,quantityArray,recipient,sendTip) {
+    async function approveToken(tokenContractAddress) {
+        //Approve token contract to spend funds
+        let tokenContract = new ethers.Contract(tokenContractAddress, erc20ABI, provider);
+        let tokenContractSigner = tokenContract.connect(signer); //Can Sign
+        let tx = await tokenContractSigner.approve(contractAddress);
+        console.log('tx', tx);
+    }
+    function moveTokens(ethValue, tokensArray, quantityArray, recipient, sendTip) {
         if (tokensArray.length != quantityArray.length) {
             throw "Arrays don´t match";
         }
-
         ethValue = !ethValue || ethValue == undefined ? 0 : ethValue;
         let overrides = {
             // To convert Ether to Wei:
             value: ethers.utils.parseEther(ethValue.toString()),
         };
-        tx = mtContractSigner.move(
-            tokensArray,
-            quantityArray,
-            recipient,
-            sendTip,
-            overrides
-        );
+        let tx = mtContractSigner.move(tokensArray, quantityArray, recipient, sendTip, overrides);
     }
-
-    function test(){
-        let arr = [0x14A7E77FbFC96e90F8A5Cbec53De86797aa67695,0x5164a7fEC539B2E54D2A2Cfb9324483F6F42DdbE]
+    function test() {
+        let arr = ["0x14A7E77FbFC96e90F8A5Cbec53De86797aa67695", "0x5164a7fEC539B2E54D2A2Cfb9324483F6F42DdbE"];
         arr.forEach(element => {
             verifyToken(element);
         });
+        approveToken(arr[1]);
     }
-
-    test();
-
-    let data = await getJSON("../ABI/movingTruck.json");
+    data = await getJSON("../ABI/movingTruck.json");
     const contractABI = data.abi;
     //console.log(contractABI)
-    mtContractReader = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        provider
-    );
+    mtContractReader = new ethers.Contract(contractAddress, contractABI, provider);
     mtContractSigner = mtContractReader.connect(signer);
     userAddress = await signer.getAddress();
-    
     let txtAddress = `Your current address is ${userAddress}, it´s correct?. Check it on <a target="_blank" href="https://etherscan.io/address/${userAddress}">Etherscan</a>`;
+    test();
     document.getElementById("lblSender").innerHTML = txtAddress;
 };
--- js/main.ts
+++ js/main.ts
@@ -1,17 +1,17 @@
 window.onload = async () => {
-    let mtContractReader;
-    let mtContractSigner;
-    let userAddress;
+    let mtContractReader:any; //Can Read
+    let mtContractSigner:any; //Can Sign
+    let userAddress:any; //Metamask address
     const contractAddress = "0x82E60f74D7F809DB197428aFcD154314d140eFA0"; //MovingTruck contract address
     //Open metamask and load
     await window.ethereum.enable();
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     //Load ERC20 ABI
-    let data = await getJSON("../ABI/ERC20Preset.json");
+    let data:any = await getJSON("../ABI/ERC20Preset.json");
     const erc20ABI = data.abi;
 
-    function getJSON(url) {
+    function getJSON(url:string) {
         //Get JSON ABI
         return new Promise((resolve, reject) => {
             fetch(url)
@@ -22,22 +22,35 @@ window.onload = async () => {
         });
     }
 
-    function verifyToken(tokenContractAddress) {
+    async function verifyToken(tokenContractAddress:string) {
         //Check available balance on specified token and approvals
         let tokenContract = new ethers.Contract(
             tokenContractAddress,
             erc20ABI,
             provider
         );
-        let name = tokenContract.name();
-        let symbol = tokenContract.symbol();
+        let name = await tokenContract.name();
+        let symbol = await tokenContract.symbol();
         let balance = await tokenContract.balanceOf(userAddress);
         balance = ethers.utils.formatUnits(balance, 18);
-        let amountApproved = tokenContract.allowance(userAddress);
+        let amountApproved = await tokenContract.allowance(userAddress, contractAddress);
         console.log(amountApproved, name, symbol, balance);
     }
 
-    function moveTokens(ethValue,tokensArray,quantityArray,recipient,sendTip) {
+    async function approveToken(tokenContractAddress:string){
+        //Approve token contract to spend funds
+        let tokenContract = new ethers.Contract(
+            tokenContractAddress,
+            erc20ABI,
+            provider
+        );
+
+        let tokenContractSigner = tokenContract.connect(signer); //Can Sign
+        let tx = await tokenContractSigner.approve(contractAddress, );
+        console.log('tx', tx);
+    }
+
+    function moveTokens(ethValue:number,tokensArray:any[],quantityArray:any[],recipient:number,sendTip:boolean) {
         if (tokensArray.length != quantityArray.length) {
             throw "Arrays don´t match";
         }
@@ -47,7 +60,7 @@ window.onload = async () => {
             // To convert Ether to Wei:
             value: ethers.utils.parseEther(ethValue.toString()),
         };
-        tx = mtContractSigner.move(
+        let tx = mtContractSigner.move(
             tokensArray,
             quantityArray,
             recipient,
@@ -57,15 +70,15 @@ window.onload = async () => {
     }
 
     function test(){
-        let arr = [0x14A7E77FbFC96e90F8A5Cbec53De86797aa67695,0x5164a7fEC539B2E54D2A2Cfb9324483F6F42DdbE]
+        let arr = ["0x14A7E77FbFC96e90F8A5Cbec53De86797aa67695","0x5164a7fEC539B2E54D2A2Cfb9324483F6F42DdbE"]
         arr.forEach(element => {
             verifyToken(element);
         });
+
+        approveToken(arr[1])
     }
 
-    test();
-
-    let data = await getJSON("../ABI/movingTruck.json");
+    data = await getJSON("../ABI/movingTruck.json");
     const contractABI = data.abi;
     //console.log(contractABI)
     mtContractReader = new ethers.Contract(
@@ -77,5 +90,6 @@ window.onload = async () => {
     userAddress = await signer.getAddress();
     
     let txtAddress = `Your current address is ${userAddress}, it´s correct?. Check it on <a target="_blank" href="https://etherscan.io/address/${userAddress}">Etherscan</a>`;
+    test();
     document.getElementById("lblSender").innerHTML = txtAddress;
 };
